4694102f19ae7210a95feb83ebe4d995d9f627a5
mcicolella <mcicolella@libero.it>
2013-04-20 23:21:37 +0300

    Update Device.java

--- src/main/java/objects/Device.java
+++ src/main/java/objects/Device.java
@@ -2,4 +2,42 @@ package objects;
 
 public class Device
 {
+  Integer basicType;
+  Integer genericType; 
+  Integer specificType; 
+  Integer infoProtocolSpecific;
+  String deviceTypeString;
+  Boolean isVirtual;
+  Boolean isListening;
+  Boolean isRouting;
+  Boolean isAwake";
+  Boolean optional;
+  Boolean isFailed;
+  Boolean beam;
+  Boolean sensor250;
+  Boolean sensor1000;
+  String neighbours; // NOTE in json it's "binary" - array of integer
+  Integer manufacturerId;
+  String vendorString;
+  Integer manufacturerProductType;
+  Integer manufacturerProductId;
+  Integer ZWLib;
+  Integer ZWProtocolMajor;
+  Integer ZWProtocolMinor;
+  String SDK;
+  Integer applicationMajor;
+  Integer applicationMinor;
+  String nodeInfoFrame; // NOTE in json it's "binary type" e.g. values [ 114, 96, 134, 143, 38, 152, 119, 32 ]
+  String ZDDXML;
+  String ZDDXMLLang;
+  String ZDDXMLFile;
+  Integer lastSend;
+  Integer lastReceived;
+  Boolean keepAwake;
+  Integer countSuccess;
+  Integer countFailed;
+  Integer queueLength;
+  String networkKey; // NOTE in json it's "binary type" e.g. values [ 114, 96, 134, 143, 38, 152, 119, 32 ]
+  Integer secureControllerId;
+
 }
